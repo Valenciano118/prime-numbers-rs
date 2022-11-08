4cf234f82a82317c7fbd9a6adaf1e48ea13a294c
Francisco de Borja Viera Prieto <borja33357@gmail.com>
2022-11-08 20:55:37 +0100

    Add eratosthenes sieve

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,108 @@
 use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc, Mutex,
-    },
+    cmp::min,
+    sync::{Arc, Mutex},
     thread::{self, JoinHandle},
     time::Instant,
 };
 
-const NUM_OF_PRIMES: u32 = 1_000_000;
+const NUM_OF_PRIMES: u64 = 1_000_000;
 fn main() {
     let instant = Instant::now();
-    single_threaded_prime(NUM_OF_PRIMES);
+    let res = single_threaded_prime(NUM_OF_PRIMES);
     let elapsed_single = instant.elapsed().as_nanos() as f64 / 1_000_000_000.0;
 
-    println!("It took {elapsed_single} seconds to calculate {NUM_OF_PRIMES} primes (single) ");
+    println!("It took {elapsed_single} seconds to calculate {res} primes (single) on the first {NUM_OF_PRIMES}  ");
 
     let instant = Instant::now();
-    multi_threaded_prime(NUM_OF_PRIMES);
+    let res = sieve_of_eratosthenes(NUM_OF_PRIMES);
+    let elapsed_sieve = instant.elapsed().as_nanos() as f64 / 1_000_000_000.0;
+
+    println!("It took {elapsed_sieve} seconds to calculate {res} primes (sieve) on the first {NUM_OF_PRIMES}  ");
+
+    let instant = Instant::now();
+    let res = multi_threaded_prime(NUM_OF_PRIMES);
     let elapsed_multi = instant.elapsed().as_nanos() as f64 / 1_000_000_000.0;
 
-    println!("It took {elapsed_multi} seconds to calculate {NUM_OF_PRIMES} primes (multi) ");
+    println!("It took {elapsed_multi} seconds to calculate {res} primes (multi) on the first {NUM_OF_PRIMES} ");
 
-    println!("The speedup was of {}", elapsed_single / elapsed_multi);
+    println!("The speedup was of {}", elapsed_sieve / elapsed_multi);
 }
 
-fn single_threaded_prime(num_of_primes: u32) {
-    let mut curr_num_of_primes = 0u32;
-    let mut curr_num = 2u32;
+fn single_threaded_prime(num_of_primes: u64) -> u64 {
+    let mut curr_num_of_primes = 0u64;
+    let mut curr_num = 0u64;
 
-    while curr_num_of_primes < num_of_primes {
+    while curr_num < num_of_primes {
         if is_prime(curr_num) {
             curr_num_of_primes += 1;
         }
         curr_num += 1;
     }
+    curr_num_of_primes
 }
 
-fn multi_threaded_prime(num_of_primes: u32) {
+fn multi_threaded_prime(num_of_primes: u64) -> u64 {
     let mut join_handler: Vec<JoinHandle<()>> = vec![];
 
-    let available_cores = std::thread::available_parallelism().unwrap().get() as u32;
+    let available_cores = std::thread::available_parallelism().unwrap().get() as u64;
+    let counter = Arc::new(Mutex::new(0));
 
-    let calculated_primes = Arc::new(Mutex::new(0u32));
-    let finished = Arc::new(AtomicBool::new(false));
-
+    let tam_bloq = (num_of_primes + available_cores - 1) / available_cores;
     for id in 0..available_cores {
-        let calculated_primes = Arc::clone(&calculated_primes);
-        let finished = Arc::clone(&finished);
+        let total_primes = Arc::clone(&counter);
         let handle = thread::spawn(move || {
-            let mut num = id * available_cores;
-            while !finished.load(Ordering::Relaxed) {
+            let start = tam_bloq * id;
+            let fin = min(start + tam_bloq, num_of_primes);
+            for num in start..fin {
                 if is_prime(num) {
-                    let mut count = calculated_primes.lock().unwrap();
-                    //println!("{num}");
-                    *count += 1;
-                    if *count == num_of_primes {
-                        finished.swap(true, Ordering::Relaxed);
-                    }
+                    let mut temp_counter = total_primes.lock().unwrap();
+
+                    *temp_counter += 1;
                 }
-                num += 1;
             }
         });
-
         join_handler.push(handle);
     }
 
     for handle in join_handler {
         handle.join().unwrap();
     }
+
+    *counter.clone().lock().unwrap()
 }
 
-fn is_prime(num: u32) -> bool {
-    let sqrt = (num as f64).sqrt() as u32;
-    if num > 1 {
-        for i in 2..sqrt {
-            if (num % i) == 0 {
-                return false;
+fn is_prime(num: u64) -> bool {
+    let sqrt = (num as f64).sqrt() as u64;
+    if num < 2 {
+        return false;
+    }
+    for i in 2..=sqrt {
+        if (num % i) == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn sieve_of_eratosthenes(n: u64) -> u64 {
+    let mut primes_list: Vec<bool> = vec![true; n as usize + 1];
+
+    let mut p = 2u64;
+
+    while p.pow(2) <= n {
+        if primes_list[p as usize] {
+            let mut i = p.pow(2);
+            while i <= n {
+                primes_list[i as usize] = false;
+                i += p
             }
         }
-        return true;
+        p += 1;
     }
-    false
+    // for (i, elem) in primes_list.clone().iter().enumerate() {
+    //     if *elem {
+    //         println!("{i} is prime");
+    //     }
+    // }
+    primes_list.iter().skip(2).filter(|&&x| x).count() as u64 //It starts calculating from 2, so we remove the first 2 elements
 }
